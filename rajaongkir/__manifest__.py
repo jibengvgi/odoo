74e39c1e1e7344719224eeb997fc52d00bad4252
Zainal Abidin <zainal@rimusoft.com>
2019-11-28 15:49:47 +0700

    init

---
+++ rajaongkir/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Raja Ongkir',
+    'version': '13.0.0',
+    'author': 'Rimusoft Teknologi Indonesia',
+    'category': 'Backend',
+    'website': 'https://www.rimusoft.com/',
+    'summary': 'Raja Ongkir Integration',
+    'description': '''''',
+    'external_dependencies': {
+         #'python': ['pyjwt'],
+    },
+    'depends': [],
+    'data': [],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
